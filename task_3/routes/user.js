3ea02b65e5df645b8735fb1f064b0911d5b16418
HIRRA ZAHID <110446345+HirraZahid@users.noreply.github.com>
2024-05-17 10:05:05 +0500

    Delete task_3/routes directory

--- task_3/routes/user.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { handleUsersSignUp } = require("../controller/user");
-const { handleUsersLogin } = require("../controller/user");
-const router = express.Router();
-router.post("/", handleUsersSignUp);
-router.post("/login", handleUsersLogin);
-module.exports = router;
